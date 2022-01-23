19eee66f81fc6b3d45652e4457de9f72fa974113
krabecb <60526718+krabecb@users.noreply.github.com>
2022-01-23 04:34:45 -0600

    Complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # three.js
+
+Link to tutorial: https://www.youtube.com/watch?v=6oFvqLfRnsU&t=227s
--- js/app.js
+++ js/app.js
@@ -25,29 +25,65 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix()
 })
 
+let raycaster = new THREE.Raycaster()
+let mouse = new THREE.Vector2()
+
 //Use Three.js' primitive shapes
 //Two things to consider: form and texture
 //(size, width, height)
 let geometry = new THREE.BoxGeometry(1, 1, 1)
-let material = new THREE.MeshLambertMaterial({color: 0xFFCC00})
+let material = new THREE.MeshLambertMaterial({color: 0xF7F7F7})
 
 //Combine geometry and material variables into a mesh
-let mesh = new THREE.Mesh(geometry, material)
+// let mesh = new THREE.Mesh(geometry, material)
 
-scene.add(mesh)
+// scene.add(mesh)
+
+let meshX = -10
+for(let i = 0; i < 15; i++) {
+    let mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = (Math.random() - 0.5) * 10
+    mesh.position.y = (Math.random() - 0.5) * 10
+    mesh.position.z = (Math.random() - 0.5) * 10
+    scene.add(mesh)
+    meshX += 1
+}
 
 //Add a light
 //(color, intensity, distance)
-let light = new THREE.PointLight(0xFFFFFF, 1, 500)
-light.position.set(10, 0, 25)
+let light = new THREE.PointLight(0xFFFFFF, 1, 1000)
+light.position.set(0, 0, 0)
 scene.add(light)
+
+let lightTwo = new THREE.PointLight(0xFFFFFF, 2, 1000)
+lightTwo.position.set(0, 0, 25)
+scene.add(lightTwo)
 
 let render = () => {
     //Draw scene every time screen is refreshed - keeps shape proportions from adjusting
     //when browser size changes
     requestAnimationFrame(render)
-    mesh.rotation.x += 0.01
     renderer.render(scene, camera)
 }
 
+function onMouseMove(event) {
+    event.preventDefault()
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1
+    mouse.y =  - ( event.clientY / window.innerHeight ) * 2 + 1
+
+    raycaster.setFromCamera(mouse, camera)
+
+    let intersects = raycaster.intersectObjects(scene.children, true)
+    for(let i = 0; i < intersects.length; i++) {
+        //Green sock animation plugin
+        this.tl = new TimelineMax()
+        this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5")
+    }
+}
+
 render()
+
+window.addEventListener('mousemove', onMouseMove)
