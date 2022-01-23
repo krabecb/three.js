1df8c4e0c12dabafd81f9e3f8641f5d211878b36
krabecb <60526718+krabecb@users.noreply.github.com>
2022-01-23 03:09:01 -0600

    Shape and light added to scene

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ let camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
+camera.position.z = 5
 
 //Set up WebGL renderer, most flexible renderer without limitations
 let renderer = new THREE.WebGLRenderer({antialias: true})
@@ -21,8 +22,24 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight)
     //Readjust aspect ratio
     camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectMatrix()
+    camera.updateProjectionMatrix()
 })
 
-renderer.render(scene, camera)
+//Use Three.js' primitive shapes
+//Two things to consider: form and texture
+//(size, width, height)
+let geometry = new THREE.SphereGeometry(1, 10, 10)
+let material = new THREE.MeshLambertMaterial({color: 0xFFCC00})
 
+//Combine geometry and material variables into a mesh
+let mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+//Add a light
+//(color, intensity, distance)
+let light = new THREE.PointLight(0xFFFFFF, 1, 500)
+light.position.set(10, 0, 25)
+scene.add(light)
+
+renderer.render(scene, camera)
